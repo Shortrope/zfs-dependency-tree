94acb78822444f8b2dadb9f65f79840b80f582ff
Mario Kaack <mario@antsle.com>
2018-11-30 19:20:59 -0800

    antlet_dependencies.py: instead of tuples use dict's

--- src/antlet_dependencies.py
+++ src/antlet_dependencies.py
@@ -26,29 +26,39 @@ def add_parents(zlist):
             for bytes in lister.stdout:
                 parent = bytes.decode().strip()
                 if parent in zlist:
-                    item = (item, zlist.index(parent))
+                    item = dict(name=item, parent_index=zlist.index(parent))
                 elif "@" in item:
                     parent = item.partition('@')[0]
-                    item = (item, zlist.index(parent))
+                    item = dict(name=item, parent_index=zlist.index(parent))
                 else:
-                    item = (item, None)
+                    item = dict(name=None)
+
                 new_zlist.append(item)
                 
     return new_zlist
 
 
 def add_children(zlist_w_parents):
+
     new_zlist = []
 
-    for item in zlist_w_parents:
+    # iterate thru list
+    for parent_item in zlist_w_parents:
         children = []
-        item_index = zlist_w_parents.index(item)
-        for i in zlist_w_parents:
-            if i[1] == item_index:
-                child_index = zlist_w_parents.index(i)
+        # get this items index as index_of_parent
+        index_of_parent = zlist_w_parents.index(parent_item)
+        # iterate thru list
+        for child_item in zlist_w_parents:
+            if child_item.__contains__('parent_index') and child_item['parent_index'] == index_of_parent:
+                # get this child_index
+                child_index = zlist_w_parents.index(child_item)
+                # append this child_index to children list
                 children.append(child_index)
 
-        new_zlist.append((item[0], item[1], children))
+        #new_zlist.append((item[0], item[1], children))
+        parent_item.update({'children': children})
+        new_zlist.append(parent_item)
+
 
     return new_zlist
 
@@ -59,7 +69,9 @@ def main():
     zfs_list = get_zfs_list()
     zfs_list_w_parents = add_parents(zfs_list)
     zfs_list_w_children = add_children(zfs_list_w_parents)
+
     #pp(zfs_list)
+    #pp(zfs_list_w_parents)
     pp(zfs_list_w_children)
 
 
