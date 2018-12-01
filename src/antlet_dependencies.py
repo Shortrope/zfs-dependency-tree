f52730f425daab8dc901b5bb3b9825a607438243
Mario Kaack <mario@antsle.com>
2018-11-30 20:19:18 -0800

    antlet_dependencies.py: remove commented tshooting lines

--- src/antlet_dependencies.py
+++ src/antlet_dependencies.py
@@ -41,20 +41,14 @@ def add_children(zlist_w_parents):
 
     new_zlist = []
 
-    # iterate thru list
     for parent_item in zlist_w_parents:
         children = []
-        # get this items index as index_of_parent
         index_of_parent = zlist_w_parents.index(parent_item)
-        # iterate thru list
         for child_item in zlist_w_parents:
             if child_item.__contains__('parent_index') and child_item['parent_index'] == index_of_parent:
-                # get this child_index
                 child_index = zlist_w_parents.index(child_item)
-                # append this child_index to children list
                 children.append(child_index)
 
-        #new_zlist.append((item[0], item[1], children))
         parent_item.update({'children': children})
         new_zlist.append(parent_item)
 
@@ -72,26 +66,9 @@ def main():
     zfs_list_w_parents = add_parents(zfs_list)
     zfs_list_w_children = add_children(zfs_list_w_parents)
 
-    #pp(zfs_list)
-    #pp(zfs_list_w_parents)
-    #pp(zfs_list_w_children)
-
-    #for i, v in enumerate(zfs_list):
-        #print("{}: {}".format(i, v))
-        #if i == 5:
-            #break
-
-    #print("")
-    #for i, v in enumerate(zfs_list_w_parents):
-        #print("{}: {}".format(i, v))
-        #if i == 5:
-            #break
-
     print("")
     for i, v in enumerate(zfs_list_w_children):
         print("{}: {}".format(i, v))
-        #if i == 5:
-            #break
 
 
 if __name__ == '__main__':
