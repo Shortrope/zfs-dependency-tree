2d5dca43eddcb021072d8a8085b7a59fef7f24d6
Mario Kaack <mario@antsle.com>
2018-12-01 14:05:56 -0800

    zfs_dependencies.py: refactoring get_zfs_list()
    Start by getting zfs list w name,origin
    Populate the empty parents (origins) in the list

--- src/zfs_dependencies.py
+++ src/zfs_dependencies.py
@@ -6,54 +6,53 @@ from pprint import pprint as pp
 
 
 # Create zfs list with snapshots
-def get_zfs_list():
+def get_zfs_list(zlist=None):
 
-    zlist = []
+    
+    if not zlist:
+        zlist = []
+        # get zfs list with snapshots and origins
+        with Popen(["zfs list -H -t filesystem,snapshot -o name,origin | grep -v antlets/_docker"],
+                    shell=True, stdout=PIPE) as lister:
 
-    with Popen(["zfs list -H -t all -o name | grep -v antlets/_docker"], shell=True, stdout=PIPE) as lister:
-        for bytes in lister.stdout:
-            line = bytes.decode().strip()
-            zlist.append(line)
+            for bytes in lister.stdout:
+                line = bytes.decode().strip()
+                zlist.append(line)
+
+    # convert each name,origin string into a map
+    for index, item in enumerate(zlist):
+        name, _, origin = item.partition('\t')
+        zlist[index] = {'name':name, 'parent': origin}
+
+    # populate 'parent' value for each zfs item
+    for item in zlist:
+        if item['parent'] != '-':
+            continue
+        elif '@' in item['name']:
+            zfs = item['name'].partition('@')[0]
+            item['parent'] = zfs
+        else:
+            item['parent'] = None
 
     return zlist
 
 
-def add_parents(zlist):
-    new_zlist = []
-    for item in zlist:
-        with Popen("zfs list -H -o origin {}".format(item), shell=True, stdout=PIPE) as lister:
-            for bytes in lister.stdout:
-                parent = bytes.decode().strip()
-                if parent in zlist:
-                    item = dict(zfs=item, parent_index=zlist.index(parent))
-                elif "@" in item:
-                    parent = item.partition('@')[0]
-                    item = dict(zfs=item, parent_index=zlist.index(parent))
-                else:
-                    item = dict(zfs=item, parent_index=None)
-
-                new_zlist.append(item)
-                
-    return new_zlist
-
 
 def add_children(zlist_w_parents):
 
-    new_zlist = []
-
-    for parent_item in zlist_w_parents:
+    for item in zlist_w_parents:
         children = []
-        index_of_parent = zlist_w_parents.index(parent_item)
+        index_of_parent = zlist_w_parents.index(item)
         for child_item in zlist_w_parents:
             if child_item.__contains__('parent_index') and child_item['parent_index'] == index_of_parent:
                 child_index = zlist_w_parents.index(child_item)
                 children.append(child_index)
 
-        parent_item.update({'children': children})
-        new_zlist.append(parent_item)
+        item.update({'children': children})
+        new_zlist.append(item)
 
 
-    return new_zlist
+    return zlist_w_parents
 
 
 def show_dependency_tree(zfs, zlist):
@@ -62,12 +61,14 @@ def show_dependency_tree(zfs, zlist):
 def main():
 
     zfs_list = get_zfs_list()
-    zfs_list_w_parents = add_parents(zfs_list)
-    zfs_list_w_children = add_children(zfs_list_w_parents)
+    #zfs_list_w_parents = add_parents(zfs_list)
+    #zfs_list_w_children = add_children(zfs_list_w_parents)
+#
+    #print("")
+    #for i, v in enumerate(zfs_list_w_children):
+        #print("{}: {}".format(i, v))
 
-    print("")
-    for i, v in enumerate(zfs_list_w_children):
-        print("{}: {}".format(i, v))
+    pp(zfs_list)
 
 
 if __name__ == '__main__':
